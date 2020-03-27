59088181886334b8b193410e27fa99cbaffd72e0
nwolczynski <nwolczynski@gmail.com>
2020-03-26 21:02:06 -0400

    made progress on imbalance bars, added fast_ewma.py, credit to  maks-ivanov

--- TAQ.py
+++ TAQ.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 import sklearn
 import datetime
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+from numba import jit
+from numba import float64
+from numba import int64
+from fast_ewma import _ewma
 
 class TAQ():
     """TAQ object generated from WRDS database TAQ trade csv file.
@@ -16,6 +22,9 @@ class TAQ():
 
         self.data = self.preprocess()
         self.timeBars = self.makeTimeBars()
+        self.Ts, self.abs_thetas, self.thresholds, self.i_s = self.compute_Ts(self.data.ticks,
+                                                                         2000,
+                                                                         abs(self.data.ticks.mean()))
 
     def make_timestamp(self, level = 'Min'):
         combined = self.rawData['DATE'].apply(str) + ' ' + self.rawData['TIME_M']
@@ -28,7 +37,11 @@ class TAQ():
         data['priceXvolume'] = data['price'] * data['volume']
         data['timestamp'] = self.make_timestamp(level)
         data['date'] = data['timestamp'].dt.date
-        data['ticks'] = (data['price'].diff()/abs(data['price'].diff())).fillna(method = 'ffill').fillna(1)
+        ticks = data['price'].diff()/abs(data['price'].diff())
+        newSym = data['ticker'].ne(data['ticker'].shift(1))
+        ticks[newSym] = 0
+        ticks = ticks.fillna(method = 'ffill')
+        data['ticks'] = ticks
         return data
 
 
@@ -44,9 +57,46 @@ class TAQ():
         c = groupData['price'].last()
         high = groupData['price'].max()
         low = groupData['price'].min()
-        frame = {'volume': volsum, 'vwap': vwap, 'open': o, 'close': c, 'high': high, 'low': low}
+        frame = {'Volume': volsum, 'VWAP': vwap, 'Open': o, 'Close': c, 'High': high, 'Low': low, 'numTicks': groupData['price'].count()}
         frame = pd.DataFrame(frame)
         return frame
+
+    def candlePlot(self, symbol, mav = None, start = None, end = None, volume = False):
+        plt.figure(figsize=(45,15))
+        plotData = self.timeBars.loc[symbol]
+        if start and end:
+            plotData = plotData.loc[start:end, :]
+        if mav:
+            mpf.plot(plotData, type = 'candle', mav = mav, volume = volume)
+        else:
+            mpf.plot(plotData, type='candle', volume = volume)
+        plt.show()
+
+    def compute_Ts(self, bvs, E_T_init, abs_Ebv_init):
+        Ts, i_s = [], []
+        i_prev, E_T, abs_Ebv = 0, E_T_init, abs_Ebv_init
+
+        n = bvs.shape[0]
+        bvs_val = bvs.values.astype(np.float64)
+        abs_thetas, thresholds = np.zeros(n), np.zeros(n)
+        abs_thetas[0], cur_theta = np.abs(bvs_val[0]), bvs_val[0]
+        for i in range(1, n):
+            cur_theta += bvs_val[i]
+            abs_theta = np.abs(cur_theta)
+            abs_thetas[i] = abs_theta
+
+            threshold = E_T * abs_Ebv
+            thresholds[i] = threshold
+
+            if abs_theta >= threshold:
+                cur_theta = 0
+                Ts.append(np.float64(i - i_prev))
+                i_s.append(i)
+                i_prev = i
+                E_T = _ewma(np.array(Ts), window=np.int64(len(Ts)))[-1]
+                abs_Ebv = np.abs(_ewma(bvs_val[:i], window=np.int64(E_T_init * 3))[-1])  # window of 3 bars
+        return Ts, abs_thetas, thresholds, i_s
+
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from TAQ import TAQ
 import pandas as pd
+import time
 
 def main():
     d = pd.read_csv('QQQ1day.csv')
+    d = d.iloc[0:1000, :]
+    now = time.time()
     taq = TAQ(data=d)
-    print(taq.data.head(-10))
+    print(taq.thresholds[0:50])
+    print(taq.Ts)
+    print(taq.i_s[0:50])
+    print("this took " + str(time.time() - now) + " seconds")
 
+    #print(taq.timeBars.head(10))
+    #print(taq.data.head(20))
+    #taq.candlePlot("QQQ", start = '2020-01-08 04:00:00', end = '2020-01-08 04:20:00', mav = 4, volume = True)
+    #print(taq.timeBars.numTicks.mean())
+    #Ts, abs_thetas, thresholds, i_s = taq.compute_Ts(taq.data.ticks, 200, abs(taq.data.ticks.mean()))
 
 
 
