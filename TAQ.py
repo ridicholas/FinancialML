c1a6ae5cf609c55e2ddeb75c08cb9f9db98298cf
nwolczynski <nwolczynski@gmail.com>
2020-04-13 21:11:33 -0400

    fixed runs method, added fixed time horizon method

--- TAQ.py
+++ TAQ.py
@@ -4,24 +4,77 @@ import sklearn
 import datetime
 import matplotlib.pyplot as plt
 import mplfinance as mpf
-from numba import jit
-from numba import float64
-from numba import int64
+
 
 
 class TAQ():
     """TAQ object generated from WRDS database TAQ trade csv file.
     Stores initial CSV as pd dataframe and renames columns"""
 
-    def __init__(self, path='null', data=None):
+    def __init__(self, path='null', data=None, ticker = None):
+
         if path != 'null':
             self.taqPath = path
             self.rawData = pd.read_csv(path)
         elif data is not None:
             self.rawData = data
 
+        if ticker == None:
+            first = self.rawData.SYM_ROOT[0]
+            self.rawData = self.rawData[self.rawData.SYM_ROOT == first]
+        else:
+            self.rawData = self.rawData[self.rawData.SYM_ROOT == ticker].reset_index(drop=True)
+
+
         self.data = self.preprocess()
-        self.Bars = self.makeBars()
+        self.timeBars = self.makeBars()
+        self.groupBars = None
+
+    def makeGroup(self, func, ET_init, type = 'dollar', wordy = False, plotty = False):
+        Ts, i_s, imbalances, abs_thetas, thresh, thetas, ticks = func(ET_init, type)
+
+        #show results
+        if wordy:
+            print(Ts)
+            print(i_s)
+            print(imbalances)
+            print(thresh)
+            print(abs_thetas)
+
+        #plot results
+        if plotty:
+            plt.plot(abs_thetas)
+            for num in i_s:
+                plt.axvline(x=num, alpha = 0.2, color = 'gray')
+            plt.title('Final Expected Runs')
+            plt.ylabel('Cumulative Dollars')
+            plt.xlabel('Index')
+            plt.show()
+            plt.plot((self.data.ticks * self.data.volume * self.data.price).cumsum())
+            plt.title('Dollar Cumulative Sum Over Time')
+            plt.ylabel('Cumulative Dollars')
+            plt.xlabel('Index')
+            for num in i_s:
+                plt.axvline(x=num, alpha = 0.2, color = 'gray')
+            plt.show()
+            plt.plot((self.data.ticks * self.data.volume * self.data.price).cumsum())
+            plt.title('Dollar Cumulative Sum Over Time')
+            plt.ylabel('Cumulative Dollars')
+            plt.xlabel('Index')
+
+            plt.show()
+
+        prev_i = 0
+        group = 0
+        for i in i_s:
+            if i == 0:
+                continue
+            else:
+                self.data.groups[prev_i:i] = group
+                prev_i = i
+                group+=1
+        self.groupBars = self.makeBars(group=True)
+
 
     def make_timestamp(self, level = 'Min'):
         combined = self.rawData['DATE'].apply(str) + ' ' + self.rawData['TIME_M']
@@ -39,14 +92,19 @@ class TAQ():
         ticks[newSym] = 0
         ticks = ticks.fillna(method = 'ffill')
         data['ticks'] = ticks
-        return data
+        data = data[data.ticks != 0]
+        data['groups'] = np.zeros(data.shape[0])
+        return data.reset_index(drop=True)
 
 
 
     def makeBars(self, group = False):
         """Will create TimeBars using level specified in pre-processing"""
         if group == True:
-            groupData = self.data.groupby(['ticker', 'group'])
+            if self.data.groups.sum() > 0:
+                groupData = self.data.groupby(['ticker', 'groups'])
+            else:
+                print('there are no groups to groupby')
         else:
             groupData = self.data.groupby(['ticker', 'timestamp'])
         volsum = groupData['volume'].sum()
@@ -56,13 +114,33 @@ class TAQ():
         c = groupData['price'].last()
         high = groupData['price'].max()
         low = groupData['price'].min()
-        frame = {'Volume': volsum, 'VWAP': vwap, 'Open': o, 'Close': c, 'High': high, 'Low': low, 'numTicks': groupData['price'].count()}
+        startTime = groupData['timestamp'].first()
+        endTime = groupData['timestamp'].last()
+        frame = {'Volume': volsum,
+                 'VWAP': vwap,
+                 'Open': o,
+                 'Close': c,
+                 'High': high,
+                 'Low': low,
+                 'numTicks': groupData['price'].count(),
+                 'startTime': startTime,
+                 'endTime': endTime}
+
+
         frame = pd.DataFrame(frame)
+        if group == True:
+            arrays = [frame.index.get_level_values(0),frame.startTime]
+            tuples = list(zip(*arrays))
+            index = pd.MultiIndex.from_tuples(tuples, names=['ticker','timestamp'])
+            frame.index = index
         return frame
 
-    def candlePlot(self, symbol, mav = None, start = None, end = None, volume = False):
+    def candlePlot(self, type = 'time', mav = None, start = None, end = None, volume = False):
         plt.figure(figsize=(45,15))
-        plotData = self.Bars.loc[symbol]
+        if type == 'group':
+            plotData = self.groupBars.droplevel(axis = 0, level = 0)
+        else:
+            plotData = self.timeBars.droplevel(axis = 0, level = 0)
         if start and end:
             plotData = plotData.loc[start:end, :]
         if mav:
@@ -71,9 +149,9 @@ class TAQ():
             mpf.plot(plotData, type='candle', volume = volume)
         plt.show()
 
-    def identifyImbalanceIndexes(self, ticker, ET_init, type = 'dollar'):
+    def identifyImbalanceIndexes(self, ET_init, type = 'dollar'):
         ET = ET_init
-        ticker = self.data[self.data.ticker == ticker]
+        ticker = self.data
 
         if type == 'tick':
             ticks = ticker.ticks
@@ -81,6 +159,7 @@ class TAQ():
             ticks = ticker.ticks * ticker.volume
         else:
             ticks = ticker.ticks * ticker.volume * ticker.price
+
 
         Eimbalance = abs(ticks.mean())
         thetas = ticks.cumsum()
@@ -97,17 +176,17 @@ class TAQ():
 
         i = 0
         while i < n:
-            barBools = abs_thetas < pd.Series(thresholds).ewm(com=0.5).mean().iloc[-1]
-            if sum(barBools) == n:
+            barBools = abs_thetas >= thresholds[-1]
+            if sum(barBools) == 0:
+                T.append(n - i_s[-1])
+                i_s.append(n)
+                imbalances.append(abs(ticks[i_s[-1]:i].mean()))
                 break
-            i = barBools[barBools == False].index[0]+1
+            i = barBools[barBools == True].index[0]+1
             imbalances.append(abs(ticks[i_s[-1]:i].mean()))
-            if len(T) > 0:
-                T.append(i - i_s[-1])
-                #if imbalances[-1] > 10*Eimbalance:
-                    #imbalances[-1] = imbalances[-2].copy()
-            else:
-                T.append(i)
+
+            T.append(i - i_s[-1])
+
 
             abs_thetas[i-1:] = thetas[i-1:] - thetas[i-1]
             abs_thetas[i_s[-1]:i] = 0
@@ -117,19 +196,18 @@ class TAQ():
             abs_thetas = abs(abs_thetas)
             final_abs_thetas[i:] = abs_thetas[i:]
             i_s.append(i)
-            ET = pd.Series(T).ewm(com=0.5).mean().iloc[-1]
+            ET = pd.Series(T).ewm(com=0.1).mean().iloc[-1]
 
-            Eimbalance = pd.Series(imbalances).ewm(com=0.5).mean().iloc[-1]
-            threshold = T[-1] * imbalances[-1]
-            thresholds.append(threshold)
+            Eimbalance = pd.Series(imbalances).ewm(com=0.1).mean().iloc[-1]
+            thresholds.append(ET * Eimbalance)
 
         return T, i_s, imbalances, final_abs_thetas, thresholds, thetas, ticks
 
 
 
-    def identifyRunsIndexes(self, ticker, ET_init, type = 'dollar'):
+    def identifyRunsIndexes(self, ET_init, type = 'dollar'):
         ET = ET_init
-        ticker = self.data[self.data.ticker == ticker]
+        ticker = self.data
 
         if type == 'tick':
             ticks = ticker.ticks
@@ -146,10 +224,9 @@ class TAQ():
         df = pd.DataFrame({"pos": posticks.cumsum(), "neg": -negticks.cumsum()})
         thetas = df[["pos","neg"]].max(axis=1)
         abs_thetas = abs(thetas)
-        Eimbalance = max(posticks[0:ET_init].mean(),abs(negticks[0:ET_init].mean()))
+        Eimbalance = max(posticks[0:ET_init][posticks < posticks[0:ET_init].quantile(.99)].mean(),abs(negticks[0:ET_init][negticks > negticks[0:ET_init].quantile(.01)].mean()))
         final_abs_thetas = abs_thetas.copy(deep=True)
         n = ticks.shape[0]
-        avg = sum(posticks/n)
         T = []
         i_s = [0]
         imbalances = []
@@ -160,34 +237,60 @@ class TAQ():
 
         i = 0
         while i < n:
-            barBools = abs_thetas < startThreshold
-            if sum(barBools) == n:
-                break
-            i = barBools[barBools == False].index[0]
-            imbalances.append(max(posticks[i_s[-1]:i].mean(),abs(negticks[i_s[-1]:i].mean())))
-            if len(T) > 0:
+            barBools = abs_thetas >= thresholds[-1]
+            if sum(barBools) == 0:
                 T.append(i - i_s[-1])
-                if imbalances[-1] > 10*Eimbalance:
-                    imbalances[-1] = imbalances[-2].copy()
-            else:
-                T.append(i)
+                i_s.append(n)
+                imbalances.append(max(posticks[i_s[-1]:i].mean(),abs(negticks[i_s[-1]:i].mean())))
+                break
+            i = barBools[barBools == True].index[1]
+            imbalances.append(max(posticks[i_s[-1]:i].mean(),abs(negticks[i_s[-1]:i].mean())))
+            T.append(i - i_s[-1])
+            if len(imbalances) >= 2 and imbalances[-1] > imbalances[-2]*5:
+                imbalances[-1] = imbalances[-2]
+                #if T[-1] < T[-2]/3.5:
+                T[-1] = T[-2]
+            elif len(imbalances) == 1 and imbalances[0] > Eimbalance*5:
+                imbalances[0] = Eimbalance
+                #if T[0] < ET_init/3.5:
+                T[0] = ET_init
 
-            df.pos[i_s[-1]:i] = 0
-            df.neg[i_s[-1]:i] = 0
-            df.pos[i:] = df.pos[i:] - df.pos[i]
-            df.neg[i:] = df.neg[i:] - df.neg[i]
+
+
+            df.pos[i_s[-1]:i-1] = 0
+            df.neg[i_s[-1]:i-1] = 0
+            df.pos[i-1:] = df.pos[i-1:] - df.pos[i-1]
+            df.neg[i-1:] = df.neg[i-1:] - df.neg[i-1]
 
             abs_thetas = df[["pos","neg"]].max(axis=1)
 
             final_abs_thetas[i:] = abs_thetas[i:]
             i_s.append(i)
-            ET = pd.Series(T[max(-3, -len(T)):]).ewm(com=0.8).mean().iloc[-1]
-            Eimbalance = pd.Series(imbalances[max(-3, -len(imbalances)):]).ewm(com=0.8).mean().iloc[-1]
+
+            if sum(T) > ET_init:
+                ET = pd.Series(T).ewm(com=0.1).mean().iloc[-1]
+                Eimbalance = pd.Series(imbalances).ewm(com=0.1).mean().iloc[-1]
+
+
 
             thresholds.append(ET * Eimbalance)
-            print("T: " + str(T[-1]) + ", imbalance: " + str(imbalances[-1])+ ", Threshold: " + str(thresholds[-1]))
+            #print("T: " + str(T[-1]) + ", imbalance: " + str(imbalances[-1])+ ", Threshold: " + str(thresholds[-1]))
 
         return T, i_s, imbalances, final_abs_thetas, thresholds, thetas, ticks
+
+
+    def fixedTimeLabel(self, numBars):
+        returns = self.groupBars.VWAP
+        print(returns)
+        returns = -returns.diff(periods=-numBars)/returns
+        tau = returns.std()
+        returns[returns > tau] = 1
+        returns[returns < -tau] = -1
+        returns[(returns > -tau) & (returns < 2*tau)] = 0
+
+        print(returns)
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,51 +1,23 @@
 from TAQ import TAQ
 import pandas as pd
 import time
+import math
 import matplotlib.pyplot as plt
 
 def main():
-    d = pd.read_csv('QQQ1day.csv')
-    d = d.iloc[0:1000]
+    d = pd.read_csv('ibmaapl.csv')
+    #d = d.iloc[0:350000]
     now = time.time()
-    taq = TAQ(data=d)
+    taq = TAQ(data=d, ticker='AAPL')
+    taq.makeGroup(taq.identifyRunsIndexes, math.floor(taq.data.shape[0]*0.01), 'dollar')
+    #print(taq.data.groups)
+    #print(taq.timeBars.index)
+    #print(taq.groupBars.index)
+    taq.candlePlot(mav=4, volume=True)
+    taq.candlePlot(type = 'group', mav=4, volume=True)
+    print('this took ' + str(time.time() - now) + ' seconds')
 
-
-    #print(taq.timeBars.head(10))
-    #print(taq.data.head(20))
-    #taq.candlePlot("QQQ", start = '2020-01-08 04:00:00', end = '2020-01-08 04:20:00', mav = 4, volume = True)
-    #print(taq.timeBars.numTicks.mean())
-
-    Ts, i_s, imbalances, abs_thetas, thresh, thetas, ticks = taq.identifyImbalanceIndexes('QQQ', 200, 'tick')
-    print(Ts)
-    print(i_s)
-    print(imbalances)
-    print(thresh)
-    print(abs_thetas)
-    print('this took ' + str(time.time()-now) + ' seconds')
-    plt.plot(abs_thetas)
-    for num in i_s:
-        plt.axvline(x=num)
-    plt.show()
-    plt.plot(thetas)
-    for num in i_s:
-        plt.axvline(x=num)
-    plt.show()
-    plt.plot((taq.data.ticks).cumsum())
-    for num in i_s:
-        plt.axvline(x=num)
-    plt.show()
-
-    #abs_thetas.to_csv('abs_thetas.csv')
-    #thetas.to_csv('thetas.csv')
-
-
-
-
-
-
-
-
-
+    taq.fixedTimeLabel(2)
 
 
 
