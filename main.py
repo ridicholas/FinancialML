e515f153c8e843d20e0631aff42051320a5ed029
nwolczynski <nwolczynski@gmail.com>
2020-04-29 17:45:26 -0400

    working purged cv

--- main.py
+++ main.py
@@ -6,31 +6,54 @@ import sklearn.metrics as skm
 import math
 import matplotlib.pyplot as plt
 import purged_cv
+import sklearn
+
+
 def main():
     d = pd.read_csv('QQQ.csv')
-    #d = d.iloc[0:2000]
-    #d2 = d[::-1].reset_index(drop=True)
-    #d = pd.concat([d,d2]).reset_index(drop=True)
+    # d = d.iloc[0:2000]
+    # d2 = d[::-1].reset_index(drop=True)
+    # d = pd.concat([d,d2]).reset_index(drop=True)
 
     now = time.time()
     taq = TAQ(data=d, rm_outliers=False)
-    #stuff = taq.data.ticks*taq.data.volume*taq.data.price
-    #stuff.plot.kde()
-    #plt.show()
-    taq.makeGroup(taq.identifyRunsIndexes, ET_init = 10000, type='dollar', wordy = False, plotty=False, rm_outs = True)
-    #print(taq.data.groups)
-    #print(taq.timeBars.index)
-    #print(taq.groupBars.index)
-    #taq.candlePlot(mav=4, volume=True)
-    #taq.candlePlot(type = 'group', mav=4, volume=True)
+    # stuff = taq.data.ticks*taq.data.volume*taq.data.price
+    # stuff.plot.kde()
+    # plt.show()
+    taq.makeGroup(taq.identifyRunsIndexes, ET_init=10000, type='dollar', wordy=False, plotty=False, rm_outs=True)
+    # print(taq.data.groups)
+    # print(taq.timeBars.index)
+    # print(taq.groupBars.index)
+    # taq.candlePlot(mav=4, volume=True)
+    # taq.candlePlot(type = 'group', mav=4, volume=True)
     print('this took ' + str(time.time() - now) + ' seconds')
-    now=time.time()
+    now = time.time()
     y_train = taq.tripleBarrierLabel(5, type='time')
     X_train = taq.timeBars
     X_train = X_train.drop(['sameDay', 'startTime', 'endTime'], axis=1)
 
+    weights = []
+    weights.append(y_train.replace(to_replace=[-1, 0, 1], value=[1, 1, 1]))
+    weights.append(y_train.replace(to_replace=[-1, 0, 1], value=[2, 1, 2]))
+    weights.append(y_train.replace(to_replace=[-1, 0, 1], value=[10, 1, 2]))
+    weights.append(y_train.replace(to_replace=[-1, 0, 1], value=[2, 1, 10]))
+    weights.append(pd.Series(sklearn.utils.compute_sample_weight('balanced', y=y_train)))
+    weights.append(1 / weights[-1])
+
     folds = purged_cv.makeFolds(X_train, y_train, 5, 5)
-    purged_cv.purgedF1CV(folds, XGBClassifier(), 1)
+    result, model, weights = purged_cv.purgedPrecisionCV(folds,
+                                                  classes=[-1, 1],
+                                                  weights=[sklearn.utils.compute_class_weight(class_weight='balanced',
+                                                                                              classes=[-1, 0, 1],
+                                                                                              y=y_train)],
+                                                  min_child_weights=[0.1, 1, 10],
+                                                  max_depths=[1, 6, 10],
+                                                  gammas=[0, 0.01, 0.1],
+                                                  colsample_bytrees=[0.5, 0.8, 1],
+                                                  eval_metrics=['error'],
+                                                  learning_rates=[0.01, 0.1],
+                                                  n_estimators=[1000, 5000]
+                                                  )
 
     '''
     y_train = y_train.iloc[0:y_train.shape[0]-5]
@@ -47,12 +70,7 @@ def main():
     print(sklearn.metrics.accuracy_score(y_test,y_pred))
     '''
 
-
     print('and this took: ' + str(time.time() - now) + ' seconds')
-
-
-
-
 
 
 if __name__ == '__main__':
--- purged_cv.py
+++ purged_cv.py
@@ -1,31 +1,29 @@
 import pandas as pd
 import sklearn.metrics as skm
+import xgboost
+import time
+import sklearn
 
 
 def makeFolds(X, y, numFolds: int, purgeBars: int):
-    """
-    :type X: pandas DataFrame
-    :type y: pandas Series
-    :type purgeBars: int
-    :type numFolds: int
-    """
     X_trains = []
     X_tests = []
     y_trains = []
     y_tests = []
+    weights = []
     numItems = int(X.shape[0] / numFolds)
     startDex = 0
-    for i in range(1,numFolds+1):
+    for i in range(1, numFolds + 1):
         if startDex == 0:
-            X_test = X.iloc[startDex:numItems-purgeBars]
-            X_train = X.iloc[startDex+numItems:X.shape[0]]
+            X_test = X.iloc[startDex:numItems - purgeBars]
+            X_train = X.iloc[startDex + numItems:X.shape[0]]
 
         elif i == numFolds:
             X_test = X.iloc[startDex + purgeBars:X.shape[0]]
             X_train = X.iloc[0:startDex]
 
         else:
-            X_test = X.iloc[startDex + purgeBars:startDex + numItems-purgeBars]
+            X_test = X.iloc[startDex + purgeBars:startDex + numItems - purgeBars]
             X_train = pd.concat([X.iloc[0:startDex], X.iloc[startDex + numItems:X.shape[0]]])
 
         y_test = y.loc[X_test.index]
@@ -42,16 +40,45 @@ def makeFolds(X, y, numFolds: int, purgeBars: int):
                          'X_test': pd.Series(X_tests),
                          'y_test': pd.Series(y_tests)})
 
-def purgedF1CV(folds, model, classes, weights, tree_depths, max_leaves):
-    results = pd.Series()
-    for i in range(folds.shape[0]):
-        model.fit(folds.X_train[i], folds.y_train[i])
-        preds = model.predict(folds.X_test[i])
-        results.append(skm.f1_score(folds.y_test[i], preds, labels=classes, average='weighted'))
 
+def purgedPrecisionCV(folds, classes, weights, min_child_weights, max_depths, gammas, colsample_bytrees, eval_metrics,
+               learning_rates, n_estimators):
+    best_result = 0
+    best_model = 0
+    iter = 1
+    numIters = len(weights) * len(min_child_weights) * len(max_depths) * len(gammas) * len(colsample_bytrees) * len(
+        eval_metrics) * len(learning_rates) * len(n_estimators)
+    final_weights = 0
+    for weight in weights:
+        for min_child_weight in min_child_weights:
+            for max_depth in max_depths:
+                for gamma in gammas:
+                    for learning_rate in learning_rates:
+                        for n_estimator in n_estimators:
+                            for colsample_bytree in colsample_bytrees:
+                                for eval_metric in eval_metrics:
+                                    now = time.time()
+                                    model = xgboost.XGBClassifier(min_child_weight=min_child_weight,
+                                                                  max_depth=max_depth,
+                                                                  gamma=gamma,
+                                                                  colsample_bytree=colsample_bytree,
+                                                                  eval_metric=eval_metric,
+                                                                  n_estimators=n_estimator,
+                                                                  learning_rate=learning_rate)
+                            results = []
 
-
-
-
-
-
+                            for i in range(folds.shape[0]):
+                                model.fit(folds.X_train[i], folds.y_train[i],
+                                          sample_weight=folds.y_train[i].replace(to_replace=[-1, 0, 1], value=weight))
+                                preds = model.predict(folds.X_test[i])
+                                results.append(skm.precision_score(folds.y_test[i], preds, labels=classes, average='micro'))
+                            result = pd.Series(results).mean()
+                            print(
+                                "iteration {}/{} took {} seconds, result: {}".format(iter, numIters, time.time() - now,
+                                                                                     result))
+                            iter += 1
+                            if result > best_result:
+                                best_result = result
+                                best_model = model
+                                final_weights = weight
+    return best_result, best_model, final_weights
